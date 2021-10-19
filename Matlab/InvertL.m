3d7ea93b2ba1515b163e323a0fd8dd1aaa0d7f1b
SeanHodoriPark <89707854+SeanHodoriPark@users.noreply.github.com>
2021-10-19 09:46:58 -0700

    Add files via upload

--- Matlab/InvertL.m
+++ Matlab/InvertL.m
@@ -1,10 +1,8 @@
 function L = InvertL(L)
 % This takes lower triangular matrix L and outputs its inverse
 n = length(L);
+B = eye(n);
 for j=1:n
-    Vect =  zeros(1,n);    % vector for inverse diagonal elements of L
-    Vect(1,j) = 1/L(j,j);  % diagonal elements of the inverse are 
-                           % just inverse of original diag elemnts of L
-    L(j,:) = Vect - Vect(1,j)*L(j,1:j-1)*L(1:j-1,:);
+    L(j,:) = (B(j,:) - L(j,1:j-1)*L(1:j-1,:))/L(j,j);
 end
 end
