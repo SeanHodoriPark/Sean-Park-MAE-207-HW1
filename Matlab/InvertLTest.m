38f3db001445bdb485f7196b4c6947cf359521aa
SeanHodoriPark <89707854+SeanHodoriPark@users.noreply.github.com>
2021-10-19 09:46:06 -0700

    Add files via upload

--- Matlab/InvertLTest.m
+++ Matlab/InvertLTest.m
@@ -3,11 +3,15 @@ close all
 clear all
 
 % some random lower triangular 5x5 matrix
-A = tril(rand(5));
+A = tril(rand(7));
 
 Ainv = InvertL(A);
 
-A*Ainv
+Matlabinv = inv(A);
+
+% see what the norm of the difference between
+% InvertL and matlab inv is
+NormDiff = norm(Matlabinv-Ainv);
 
 %% After computing Ainv from the InvertL function we find that Ainv*A gives us the 
 %% identity matrix. From inspection we see that Ainv is similarly lower triangular
@@ -18,10 +22,8 @@ A*Ainv
 function L = InvertL(L)
 % This takes lower triangular matrix L and outputs its inverse
 n = length(L);
+B = eye(n);
 for j=1:n
-    Vect =  zeros(1,n);    % vector for inverse diagonal elements of L
-    Vect(1,j) = 1/L(j,j);  % diagonal elements of the inverse are 
-                           % just inverse of original diag elemnts of L
-    L(j,:) = Vect - Vect(1,j)*L(j,1:j-1)*L(1:j-1,:);
+    L(j,:) = (B(j,:) - L(j,1:j-1)*L(1:j-1,:))/L(j,j);
 end
 end
